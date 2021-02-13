91bb6593df93f3a405b23351de33bc235ad953b8
cadecr <dev@Hero-System.localdomain>
2021-02-13 14:22:21 -0800

    finished routing draft/setup

--- client/pages/homeSearch.jsx
+++ client/pages/homeSearch.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function HomeSearch() {
-  return <h1 className="text-center mt-4">Home Serach Page</h1>;
+  return (
+  <h1 className="text-center mt-4">Home Search Page</h1>
+  );
 }
